41f379004c5662212f9fd9e07478cbae487822d0
Beta Kuang <beta.kuang@gmail.com>
2015-12-16 08:45:58 +0800

    Finished basic structure of notifications activity

--- app/src/main/java/net/kyouko/cloudier/activity/NotificationsActivity.java
+++ app/src/main/java/net/kyouko/cloudier/activity/NotificationsActivity.java
@@ -1,25 +1,21 @@
 package net.kyouko.cloudier.activity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import net.kyouko.cloudier.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class NotificationsActivity extends AppCompatActivity {
 
     @Bind(R.id.coordinator)
     CoordinatorLayout coordinatorLayout;
@@ -29,26 +25,30 @@ public class MainActivity extends AppCompatActivity {
     SwipeRefreshLayout swipeRefreshLayout;
     @Bind(R.id.recycler)
     RecyclerView recyclerView;
-    @Bind(R.id.fab)
-    FloatingActionButton fab;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notifications);
 
         ButterKnife.bind(this);
 
         initToolbar();
         initSrl();
         initRecyclerView();
-        initFab();
     }
 
 
     private void initToolbar() {
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 
@@ -66,41 +66,6 @@ public class MainActivity extends AppCompatActivity {
     private void initRecyclerView() {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-    }
-
-
-    private void initFab() {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_notifications) {
-            startActivity(new Intent(this, NotificationsActivity.class));
-            return true;
-        }
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
